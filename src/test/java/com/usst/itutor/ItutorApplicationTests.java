4a1af0dc06c9ba6d270d7410c8bb655700d159a3
Percy Chen <853879506@qq.com>
2018-05-15 23:18:20 +0800

    使用springboot创建了itutor并完成了登录的操作

---
+++ src/test/java/com/usst/itutor/ItutorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.usst.itutor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ItutorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
